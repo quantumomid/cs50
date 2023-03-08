73f3d6dfcb17b0e246b4ed5c077ba497105bb4cb
Omid Wakili <omidwakili@Omids-MacBook-Pro.local>
2023-03-08 17:37:58 +0000

    feat(ps1): add credit2 form of my soln credit.c but using Chatgpt for commenting and documentation

---
+++ Week 1/ps1/credit/credit2.c
@@ -0,0 +1,188 @@
+// My solution plus ChatGPT for comments and function documentation!
+
+// Include libraries
+#include <cs50.h> // for the get_long function
+#include <stdio.h> // for the printf function
+
+// Declare function prototypes
+/**
+ * Prompts the user for a credit card number and ensures non-negative input.
+ * 
+ * @return long The credit card number provided by the user.
+ */
+long get_credit_card_number(void);
+
+/**
+ * Calculates the sum of the credit card number using Luhn's algorithm.
+ * 
+ * @param user_cc_number The credit card number provided by the user.
+ * @return int The sum of the credit card number.
+ */
+int calculate_sum_of_cc_luhns(long user_cc_number);
+
+/**
+ * Determines the length of the credit card number.
+ * 
+ * @param user_cc_number The credit card number provided by the user.
+ * @return int The length of the credit card number.
+ */
+int get_cc_length(long user_cc_number);
+
+/**
+ * Checks the validity and type of the credit card number.
+ * 
+ * @param is_valid A boolean indicating whether the credit card number is valid.
+ * @param user_cc_number The credit card number provided by the user.
+ * @param cc_length The length of the credit card number.
+ * @return int A value indicating whether the credit card number is valid (1) or invalid (0).
+ */
+int check_card_validity_and_type(bool is_valid, long user_cc_number, int cc_length);
+
+
+int main(void)
+{
+    long const user_credit_number = get_credit_card_number(); // get user credit card number input
+
+    // Determine length of the credit card number
+    int cc_length = get_cc_length(user_credit_number);
+
+    // Calculate sum using Luhn's algorithm to determine if credit card number is valid
+    int total_sum = calculate_sum_of_cc_luhns(user_credit_number);
+
+    // Determine if the credit card number is valid
+    bool is_valid = (total_sum % 10) == 0;
+
+    // Determine and print the validity and type of credit card
+    check_card_validity_and_type(is_valid, user_credit_number, cc_length);
+}
+
+// Prompts user for credit card number and returns it
+long get_credit_card_number(void)
+{
+    long user_credit_number;
+    do
+    {
+        user_credit_number = get_long("What is your credit card number? \n");
+    } while (user_credit_number < 0);
+    return user_credit_number;
+}
+
+// Calculates the sum of the credit card digits using Luhn's algorithm and returns it
+int calculate_sum_of_cc_luhns(long user_cc_number)
+{
+    long cc = user_cc_number;
+    bool is_double = false; // flag for whether to double the next digit
+    int sum_of_doubled_numbers = 0;
+    int sum_of_other_numbers = 0;
+    while (cc > 0)
+    {
+        // Get the rightmost digit
+        int last_digit = cc % 10;
+
+        // If the current digit is one that should be doubled
+        if (is_double == true)
+        {
+            int last_digit_doubled = last_digit * 2;
+
+            // If doubling creates two digits, add them individually
+            if (last_digit_doubled >= 10)
+            {
+                int first_digit = last_digit_doubled % 10; // second digit (from right)
+
+                last_digit_doubled = last_digit_doubled / 10; // To remove last digit
+
+                int second_digit = last_digit_doubled % 10; // first digit (from right)
+                sum_of_doubled_numbers += first_digit + second_digit;
+            }
+            else
+            {
+                sum_of_doubled_numbers += last_digit * 2;
+            }
+
+            // Reset flag
+            is_double = false;
+        }
+        else
+        {
+            sum_of_other_numbers += last_digit;
+
+            // Set flag
+            is_double = true;
+        }
+
+        // Remove rightmost digit from number
+        cc = cc / 10;
+    }
+
+    // Determine the total sum of the credit card digits
+    int total_sum = sum_of_doubled_numbers + sum_of_other_numbers;
+    return total_sum;
+}
+
+// Returns the length of the credit card number
+int get_cc_length(long user_cc_number)
+{
+    int cc_length = 0;
+    long cc = user_cc_number;
+    while (cc > 0)
+    {
+        // Remove rightmost digit from number
+        cc = cc / 10;
+
+        // Increment counter for number of digits processed
+        cc_length++;
+    }
+        return cc_length;
+}
+
+int check_card_validity_and_type(bool is_valid, long user_cc_number, int cc_length)
+{
+    // Print results based on validity and type of credit card
+    if (is_valid)
+    {
+        printf("The credit card %li is Valid.\n", user_cc_number);
+
+        // Check length and first digits to determine type of card
+        if (cc_length == 16)
+        {
+            int first_two_digits = user_cc_number / 100000000000000;
+            int first_digit = first_two_digits / 10; // For VISA need first digit
+            printf("First two digit of the credit card is: %i \n", first_two_digits);
+
+            if (first_two_digits >= 51 && first_two_digits <= 55)
+            {
+                printf("MASTERCARD \n");
+            }
+            else if (first_digit == 4)
+            {
+                printf("VISA \n");
+            }
+        }
+        else if (cc_length == 13)
+        {
+            int first_digit = user_cc_number / 100000000000; // For VISA need first digit
+            printf("First digit of the credit card is: %i \n", first_digit);
+
+            if (first_digit == 4)
+            {
+                printf("VISA \n");
+            }
+        }
+        else if (cc_length == 15)
+        {
+            int first_two_digits = user_cc_number / 10000000000000;
+            printf("First two digit of the credit card is: %i \n", first_two_digits);
+
+            if (first_two_digits == 34 || first_two_digits == 37)
+            {
+                printf("AMEX \n");
+            }
+        }
+        return 1;
+    }
+    else
+    {
+        printf("The credit card %li is Invalid.\n", user_cc_number);
+        return 0;
+    }
+}
